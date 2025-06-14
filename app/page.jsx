aca9568b2b66714c659d5d3a8e330961d58538cf
Kaustubh <k2080495@gmail.com>
2025-06-14 20:05:24 +0530

    install shadncn

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (  
+    <div>
+      <h2>Subscribe to </h2>
+    <Button>
+      Hello
+    </Button>
+    </div>
+  );
+}
